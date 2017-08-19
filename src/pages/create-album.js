91b957bbfb17a5deb7bcc1120e14c6c19b5e0fbd
cwebsterz <cwebsterzornes@live.com>
2017-08-19 13:42:28 -0400

    updated add feature

--- src/pages/create-album.js
+++ src/pages/create-album.js
@@ -6,71 +6,80 @@ import { createCrateAlbum } from '../db'
 import { SET_ALBUM_X } from '../constants'
 import { toUpper } from 'ramda'
 
-const CreateAlbum = props => {
-  return (
-    <div className="flex flex-column justify-start w-100">
-      <header className="h3 flex justify-between items-center bg-black-30">
-        <div className="ml3">
-          <Link className="link hover-white black-60" to="/pages/list-albums">
-            <i className="db tc ion-close" />
-          </Link>
-        </div>
-        <div className="f4">
-          <img
-            alt="Logo"
-            className="h3 w3"
-            src="https://cdn.glitch.com/011e6829-7f06-454e-8fb9-2891947cf4a7%2Fcratelogo.svg?1501867486368"
-          />
-        </div>
-        <div className="mr3">
-          <Link to="/pages/search-albums">
-            <i className="db tc black-60 hover-white ion-search" />
-          </Link>
-        </div>
-      </header>
-      <main className="overflow-scroll">
-        <h4>Add Album to Crate</h4>
+class CreateAlbum extends React.Component {
+  render() {
+    const props = this.props
+    return (
+      <div className="flex flex-column justify-start w-100">
+        <header className="h3 flex justify-between items-center bg-black-30">
+          <div className="ml3">
+            <Link className="link hover-white black-60" to="/pages/list-albums">
+              <i className="db tc ion-close" />
+            </Link>
+          </div>
+          <div className="f4">
+            <img
+              alt="Logo"
+              className="h3 w3"
+              src="https://cdn.glitch.com/011e6829-7f06-454e-8fb9-2891947cf4a7%2Fcratelogo.svg?1501867486368"
+            />
+          </div>
+          <div className="mr3">
+            <Link to="/pages/search-albums">
+              <i className="db tc black-60 hover-white ion-search" />
+            </Link>
+          </div>
+        </header>
+        <main className="overflow-scroll">
+          <h4>Add Album to Crate</h4>
 
-        <form className="ph2" onSubmit={props.handleSubmit(props.history)}>
-          <TextField
-            label="Title"
-            value={props.title}
-            onChange={props.setTitle}
-          />
-          <TextField
-            label="Artist"
-            value={props.artist}
-            onChange={props.setArtist}
-          />
-          <TextField label="Year" value={props.year} onChange={props.setYear} />
-          <TextField
-            label="Genre"
-            value={props.genre}
-            onChange={props.setGenre}
-          />
-          <TextField
-            label="Description (optional)"
-            value={props.desc}
-            onChange={props.setDesc}
-          />
-          <TextField
-            label="Photo (optional)"
-            value={props.photo}
-            onChange={props.setPhoto}
-          />
-          <div className="measure mt2">
-            <div className="flex justify-center pv4">
-              <img alt="" className="pa2 br2 mr2" src={props.photo} />
+          <form className="ph2" onSubmit={props.handleSubmit(props.history)}>
+            <TextField
+              label="Title"
+              value={props.title}
+              onChange={props.setTitle}
+            />
+            <TextField
+              label="Artist"
+              value={props.artist}
+              onChange={props.setArtist}
+            />
+            <TextField
+              label="Year"
+              value={props.year}
+              onChange={props.setYear}
+            />
+            <TextField
+              label="Genre"
+              value={props.genre}
+              onChange={props.setGenre}
+            />
+            <TextField
+              label="Description (optional)"
+              value={props.desc}
+              onChange={props.setDesc}
+            />
+            <TextField
+              label="Photo (optional)"
+              value={props.photo}
+              onChange={props.setPhoto}
+            />
+            <div className="measure mt2">
+              <div className="flex justify-center pv4">
+                <img alt="" className="pa2 br2 mr2" src={props.photo} />
+              </div>
             </div>
-          </div>
 
-          <div>
-            <Button className="w-100 bg-black white ba br2">Save Album</Button>
-          </div>
-        </form>
-      </main>
-    </div>
-  )
+            <div>
+              <Button className="w-100 bg-black white ba br2">
+                Save Album
+              </Button>
+            </div>
+          </form>
+        </main>
+      </div>
+    )
+  }
 }
 
 const connector = connect(mapStateToProps, mapActionsToProps)
@@ -96,9 +105,8 @@ function mapActionsToProps(dispatch) {
   }
   return {
     dispatch,
-    handleSubmit: history => e => {
-      e.preventDefault()
-      dispatch(createCrateAlbum(history))
+    handleSubmit: (album, history) => e => {
+      dispatch(createCrateAlbum(album, history))
     },
     setTitle: e => doDispatch('TITLE', e.target.value),
     setArtist: e => doDispatch('ARTIST', e.target.value),
